66be8768155e6c3f656a063ca3566b4b96396210
Iamneo <techadmin@iamneo.ai>
2023-10-03 20:41:45 +0000

    Backup Commit

--- boodetails/src/main/java/com/book/model/Book.java
+++ boodetails/src/main/java/com/book/model/Book.java
@@ -1,5 +1,47 @@
 package com.book.model;
 
 public class Book {
-    
+    private int id;
+    private String title;
+    private String author;
+    private int year;
+
+    public Book(){}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return "Book [author=" + author + ", id=" + id + ", title=" + title + ", year=" + year + "]";
+    }    
 }
