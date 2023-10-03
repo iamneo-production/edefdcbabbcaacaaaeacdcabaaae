f5b660a18a392a1d4d9ae49855abf58db89bdda4
Iamneo <techadmin@iamneo.ai>
2023-10-03 20:31:45 +0000

    Backup Commit

---
+++ boodetails/src/main/java/com/book/boodetails/BoodetailsApplication.java
@@ -0,0 +1,13 @@
+package com.book.boodetails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoodetailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoodetailsApplication.class, args);
+	}
+
+}
