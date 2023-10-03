df88cd249f8ad01c0ce060c95cce96b41b9b13b1
Iamneo <techadmin@iamneo.ai>
2023-10-03 20:46:45 +0000

    Backup Commit

---
+++ boodetails/src/main/java/com/book/boodetails/BookController.java
@@ -0,0 +1,13 @@
+package com.book.boodetails;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BookController {
+    
+    @GetMapping
+    public String getBooks(){
+        return "All books";
+    }
+}
