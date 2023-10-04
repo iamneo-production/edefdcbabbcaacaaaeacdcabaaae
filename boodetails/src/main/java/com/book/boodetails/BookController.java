717d9998cb7f2c6b3d8137c8be528605cd6d32d3
Iamneo <techadmin@iamneo.ai>
2023-10-04 04:46:53 +0000

    Backup Commit

--- boodetails/src/main/java/com/book/boodetails/BookController.java
+++ boodetails/src/main/java/com/book/boodetails/BookController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class BookController {
     
-    @GetMapping
+    @GetMapping("/")
     public String getBooks(){
         return "All books";
     }
